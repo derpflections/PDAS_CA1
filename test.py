406c6b35c05478267cdef8f179eb509c0ca4eb1d
derpflections <leehongyi91@gmail.com>
2022-11-30 01:38:39 +0800

    e

--- test.py
+++ test.py
@@ -1,31 +1,12 @@
 import numpy as np 
-import matplotlib as mpl
-counter = 0 
+import matplotlib.pyplot as plt
 
-secondaryData = np.genfromtxt('PDAS CA1 Files\PDAS_CA1\sourcefiles\enrolment-secondary-by-level-and-course.csv', delimiter = ',', skip_header = 1, dtype = [('year','i8'),('level','U255'),('course','U255'),('gender','U255'),('enrolment','i8')])
+year = np.arange(2008,2020,1)
+catSec = [50919, 52186, 52073, 51263, 51325, 49190, 45183, 45413, 47869, 43031, 42238, 39522]
 
-#print(f"**Analysis of course intake and enrolment for secondary schools**")
-#print(f"There are {len(secondaryData)} rows and {len(secondaryData[1])} columns in this dataset\n")
-#
-#print(f"The names and datatypes of these columns are: ")
-#print(f"- {secondaryData.dtype.names[0]} {type(secondaryData['year'][1])}, isnumeric = {np.issubdtype(type(secondaryData['year'][1]), np.number)}")
-#print(f"- {secondaryData.dtype.names[1]} {type(secondaryData['level'][1])}, isnumeric = {np.issubdtype(type(secondaryData['level'][1]), np.number)}")
-#print(f"- {secondaryData.dtype.names[2]} {type(secondaryData['course'][1])}, isnumeric = {np.issubdtype(type(secondaryData['course'][1]), np.number)}")
-#print(f"- {secondaryData.dtype.names[3]} {type(secondaryData['gender'][1])}, isnumeric = {np.issubdtype(type(secondaryData['gender'][1]), np.number)}")
-#print(f"- {secondaryData.dtype.names[4]} {type(secondaryData['enrolment'][1])}, isnumeric = {np.issubdtype(type(secondaryData['enrolment'][1]), np.number)}\n")
-#
-#print(f"There are {len(np.unique(secondaryData['year']))} unique values in the column \"{secondaryData.dtype.names[0]}\"")
-#print(f"There are {len(np.unique(secondaryData['level']))} unique values in the column \"{secondaryData.dtype.names[1]}\"")
-#print(f"There are {len(np.unique(secondaryData['course']))} unique values in the column \"{secondaryData.dtype.names[2]}\"")
-#print(f"There are {len(np.unique(secondaryData['gender']))} unique values in the column \"{secondaryData.dtype.names[3]}\"")
-#print(f"There are {len(np.unique(secondaryData['enrolment']))} unique values in the column \"{secondaryData.dtype.names[4]}\"")
+print(year)
+print(catSec)
 
-counter = 0
-for i in secondaryData[1]:
-    print(f"- {secondaryData.dtype.names[counter]} {type(secondaryData[secondaryData.dtype.names[counter]][1])}, isnumeric = {np.issubdtype(type(secondaryData[secondaryData.dtype.names[counter]][1]), np.number)}")
-    counter += 1
-
-counter = 0 
-for i in secondaryData[1]:
-    print(f"There are {len(np.unique(secondaryData[secondaryData.dtype.names[counter]]))} unique values in the column \"{secondaryData.dtype.names[counter]}\"")
-    counter += 1
+plt.xticks = (np.arange(2008, 2019, 1))
+plt.plot((2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019), catSec)
+plt.show()

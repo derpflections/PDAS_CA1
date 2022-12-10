dd99d7b7b0b185b2307861971b89d074772e5cf8
derpflections <leehongyi91@gmail.com>
2022-12-10 21:16:43 +0800

    10 dec

--- test.py
+++ test.py
@@ -1,12 +1,41 @@
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 
-year = np.arange(2008,2020,1)
-catSec = [50919, 52186, 52073, 51263, 51325, 49190, 45183, 45413, 47869, 43031, 42238, 39522]
 
-print(year)
-print(catSec)
+# First create some toy data:
+x = np.linspace(0, 2*np.pi, 400)
+y = np.sin(x**2)
 
-plt.xticks = (np.arange(2008, 2019, 1))
-plt.plot((2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019), catSec)
+# Create just a figure and only one subplot
+fig, ax = plt.subplots()
+ax.plot(x, y)
+ax.set_title('Simple plot')
+
+# Create two subplots and unpack the output array immediately
+f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+ax1.plot(x, y)
+ax1.set_title('Sharing Y axis')
+ax2.scatter(x, y)
+
+# Create four polar axes and access them through the returned array
+fig, axs = plt.subplots(2, 2, subplot_kw=dict(projection="polar"))
+axs[0, 0].plot(x, y)
+axs[1, 1].scatter(x, y)
+
+# Share a X axis with each column of subplots
+plt.subplots(2, 2, sharex='col')
+
+# Share a Y axis with each row of subplots
+plt.subplots(2, 2, sharey='row')
+
+# Share both X and Y axes with all subplots
+plt.subplots(2, 2, sharex='all', sharey='all')
+
+# Note that this is the same as
+plt.subplots(2, 2, sharex=True, sharey=True)
+
+# Create figure number 10 with a single subplot
+# and clears it if it already exists.
+fig, ax = plt.subplots(num=10, clear=True)
+
 plt.show()
